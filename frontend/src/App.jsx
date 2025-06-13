8ce06219c2de4eeb8cc6ae9c14487e12aa414bfa
pvtsonicbaka <ashiyanap1680@gmail.com>
2025-06-13 23:17:51 +0530

    added navbar login and signup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,45 @@
 
-import './App.css'
-import Navbar from './components/ui/shared/Navbar'
+import Login from './components/auth/Login'
+import Signup from './components/auth/Signup'
+import Home from './components/Home'
+import Navbar from './components/shared/Navbar'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+const appRouter = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/login',
+    element: <Login />
+  },
+  {
+    path: '/signup',
+    element: <Signup />
+  },
+  // {
+  //   path:'/',
+  //   element:<Home/>
+  // },
+  // {
+  //   path:'/',
+  //   element:<Home/>
+  // },
+  // {
+  //   path:'/',
+  //   element:<Home/>
+  // },
+
+
+]
+
+
+)
 function App() {
 
   return (
     <>
-    <Navbar/>
+      <RouterProvider router = { appRouter } />
     </>
   )
 }
---
+++ frontend/src/components/auth/Signup.jsx
@@ -0,0 +1,94 @@
+import React from 'react'
+import Navbar from '../shared/Navbar'
+import { Label } from '../ui/label'
+import { RadioGroup } from "../ui/radio-group"
+import { Input } from '../ui/input'
+import { Button } from '../ui/button'
+import { Link } from 'react-router-dom'
+const Signup = () => {
+    return (
+        <div>
+            <Navbar />
+            <div className='flex items-center justify-center max-w-7xl mx-auto'>
+                <form action="" className='w-1/2 border border-gray-200 rounded-md p-4 my-10'>
+                    <h1 className='font-bold text-xl mb-5'> Sign up</h1>
+                    <div className='my-2'>
+                        <Label>
+                            Full name
+                        </Label>
+                        <input
+                            type='text'
+                            placeholder='mr someone who idk'
+                        />
+                    </div>
+                    <div className='my-2'>
+                        <Label>
+                            Email
+                        </Label>
+                        <input
+                            type='email'
+                            placeholder='pakistan@gmail.com'
+                        />
+                    </div>
+                    <div className='my-2'>
+                        <Label>
+                            Phone no.
+                        </Label>
+                        <input
+                            type='text'
+                            placeholder='idk'
+                        />
+
+                    </div>
+                    <div className='my-2'>
+                        <Label>
+                            Password
+                        </Label>
+                        <input
+                            type='password'
+                            placeholder='idk'
+                        />
+                    </div>
+                    <div className='flex items-center justify-between '>
+                        <RadioGroup className="flex items-center gap-4 my-5">
+                            <div className="flex items-center gap-3">
+                                <Input
+                                    type='radio'
+                                    name="role"
+                                    value="student"
+                                    className="cursor-pointer"
+                                />
+                                <Label htmlFor="r1">Student</Label>
+                            </div>
+                            <div className="flex items-center gap-3">
+                                <Input
+                                    type='radio'
+                                    name="role"
+                                    value="recuriter"
+                                    className="cursor-pointer"
+                                />
+                                <Label htmlFor="r2">Recuriter</Label>
+                            </div>
+
+                        </RadioGroup>
+                        <div className='flex items-center gap-2'>
+                            <Label>Profile</Label>
+                            <Input 
+                            accept="image/*"
+                            type="file"
+                            className="cursor-pointer"
+                            />
+                        </div>
+                    </div>
+                    <Button type="submit" className="w-full my-4">
+                        Signup
+                    </Button>
+                    <span className='text-sm'>Already have an account? <Link to="/login" className='text-blue-600' >Login</Link></span>
+                </form>
+
+            </div>
+        </div>
+    )
+}
+
+export default Signup
